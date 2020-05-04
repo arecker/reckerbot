6360b13f09fb53c85cebbb628d3c3bf3283e2a9b
Alex Recker <alex@reckerfamily.com>
2020-05-04 10:48:42 -0500

    Add buildx script

---
+++ scripts/buildx.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+
+VERSION="$(cat VERSION)"
+IMAGE_NAME="arecker/reckerbot"
+PLATFORMS="linux/386,linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
+
+docker buildx build \
+       --platform "$PLATFORMS" \
+       --output "type=image,push=true" \
+       --tag "${IMAGE_NAME}:${VERSION}" \
+       --tag "${IMAGE_NAME}:latest" \
+       --file Dockerfile .
